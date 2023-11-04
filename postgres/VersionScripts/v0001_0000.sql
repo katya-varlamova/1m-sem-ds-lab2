692f460f29115c0df22cde69d0b735eaca5e6375
Ekaterina_Varlamova <katy1781@inbox.ru>
2023-11-05 01:17:12 +0300

    added postgres scripts

--- postgres/VersionScripts/v0001_0000.sql
+++ postgres/VersionScripts/v0001_0000.sql
@@ -11,10 +11,15 @@ CREATE TABLE ticket
         CHECK (status IN ('PAID', 'CANCELED'))
 );
 
-INSERT INTO ticket (id, ticket_uid ,username, flight_number, price, status) VALUES
-(1, '049161bb-badd-4fa8-9d90-87c9a82b0668', 'name', 'AFL031', 1500, 'PAID');
-
 \c flights
+
+CREATE TABLE airport
+(
+    id      SERIAL PRIMARY KEY,
+    name    VARCHAR(255),
+    city    VARCHAR(255),
+    country VARCHAR(255)
+);
 
 CREATE TABLE flight
 (
@@ -26,25 +31,6 @@ CREATE TABLE flight
     price           INT                      NOT NULL
 );
 
-INSERT INTO flight (id, flight_number, datetime, from_airport_id, to_airport_id, price) VALUES
-(1, 'AFL031', '2021-10-08 20:00', 2, 1, 1500);
-
-
-
-CREATE TABLE airport
-(
-    id      SERIAL PRIMARY KEY,
-    name    VARCHAR(255),
-    city    VARCHAR(255),
-    country VARCHAR(255)
-);
-
-INSERT INTO airport (id, name, city, country) VALUES
-(1, 'Шереметьево', 'Москва', 'Россия');
-INSERT INTO airport (id, name, city, country) VALUES
-(2, 'Пулково', 'Санкт-Петербург', 'Россия');
-
-
 
 \c privileges
 CREATE TABLE privilege
@@ -55,8 +41,6 @@ CREATE TABLE privilege
         CHECK (status IN ('BRONZE', 'SILVER', 'GOLD')),
     balance  INT
 );
-INSERT INTO privilege (id, username,status, balance) VALUES
-(1, 'name', 'GOLD', 1500);
 
 CREATE TABLE privilege_history
 (
@@ -69,9 +53,3 @@ CREATE TABLE privilege_history
         CHECK (operation_type IN ('FILL_IN_BALANCE', 'DEBIT_THE_ACCOUNT'))
 );
 
-INSERT INTO privilege_history (privilege_id, ticket_uid, datetime, balance_diff, operation_type) VALUES
-(1,
- 'name',
- '049161bb-badd-4fa8-9d90-87c9a82b0668',
- '2021-10-08T19:59:19Z',
- 1500);
