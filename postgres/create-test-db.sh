fccf6e586cf33bb268591babf8247d7b6f825297
Ekaterina_Varlamova <katy1781@inbox.ru>
2023-11-06 16:42:20 +0300

    postgres fix

--- postgres/create-test-db.sh
+++ postgres/create-test-db.sh
@@ -2,4 +2,5 @@
 set -e
 
 psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < scripts/db-v1.sql
+psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < VersionScripts/v0001_0000.sql
 psql "user=postgres hostaddr=127.0.0.1 port=5432 password=postgres" < 30-fill-test-DB.sql
